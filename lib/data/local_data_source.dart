01d97bb2c2a23cb4d6b2297d46678251b0b3562b
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-04-08 21:45:43 +0900

    WebSocketのホストとポートを読み書きできるようにした。

--- lib/data/local_data_source.dart
+++ lib/data/local_data_source.dart
@@ -4,6 +4,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 class LocalDataSource {
   static const _keyCameraHasStarted = "cameraHasStarted";
 
+  static const _keyWebSocketHost = "webSocketHost";
+
+  static const _keyWebSocketPort = "webSocketPort";
+
   Future<void> setCameraHasStarted(bool cameraHasConnected) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool(_keyCameraHasStarted, cameraHasConnected);
@@ -12,6 +16,26 @@ class LocalDataSource {
   Future<bool> isCameraHasStarted() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getBool(_keyCameraHasStarted) ?? false;
+  }
+
+  Future<void> setWebSocketHost(String webSocketHost) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_keyWebSocketHost, webSocketHost);
+  }
+
+  Future<String> getWebSocketHost() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_keyWebSocketHost) ?? "localhost";
+  }
+
+  Future<void> setWebSocketPort(int webSocketPort) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_keyWebSocketPort, webSocketPort);
+  }
+
+  Future<int> getWebSocketPort() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(_keyWebSocketPort) ?? 4455;
   }
 }
 
--- test/data/local_data_source_test.dart
+++ test/data/local_data_source_test.dart
@@ -9,10 +9,18 @@ void main() {
     final container = ProviderContainer();
 
     final localDataSource = container.read(localDataSourceProvider);
+    // デフォルト設定の確認
     expect(await localDataSource.isCameraHasStarted(), false);
+    expect(await localDataSource.getWebSocketHost(), "localhost");
+    expect(await localDataSource.getWebSocketPort(), 4455);
+    // 保存の確認
     await localDataSource.setCameraHasStarted(true);
     expect(await localDataSource.isCameraHasStarted(), true);
     await localDataSource.setCameraHasStarted(false);
     expect(await localDataSource.isCameraHasStarted(), false);
+    await localDataSource.setWebSocketHost("example.com");
+    expect(await localDataSource.getWebSocketHost(), "example.com");
+    await localDataSource.setWebSocketPort(4455);
+    expect(await localDataSource.getWebSocketPort(), 4455);
   });
 }
